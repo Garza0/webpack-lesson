e2838811c6acedfa5cdbd926e9aec2af7e170cd5
Garza0 <konovalovandrey7@gmail.com>
2020-01-18 21:00:04 +0200

    plugins

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as $ from 'jquery'
+import Post from '@models/Post'
+import json from './assets/json'
+import WebpackLogo from '@/assets/webp'
+import './styles/styles.css'
+const post = new Post('Webpack Post Title', WebpackLogo)
+
+console.log('Post To string:', post.toString())
+
+console.log('json', json)
